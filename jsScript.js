7f5526ed75b535c65482b42d7992d5bd61ca2fc1
Ryan Nkansah <ryannkan@gmail.com>
2023-07-30 13:41:49 -0400

    buttons function well

--- jsScript.js
+++ jsScript.js
@@ -1,9 +1,57 @@
+const buttonRock = document.querySelector('#rock');
+const buttonPaper = document.querySelector('#paper');
+const buttonScissors = document.querySelector('#scissors');
+const body = document.querySelector('body')
+
+
+buttonRock.addEventListener('click', () => {
+  playerSelection = 'rock'
+  game()
+});
+buttonPaper.addEventListener('click', () => {
+  playerSelection = 'paper'
+  game()
+})
+buttonScissors.addEventListener('click', () => {
+  playerSelection = 'scissors'
+  game()
+})
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let humanwins = 0
+let compwins = 0
 
 let counter = 0;
    
@@ -27,7 +75,7 @@ function game() {
   function playRound(playerSelection, computerSelection) {
     console.log(`computer chose:  ${computerSelection}`)
     console.log(`player chose: ${playerSelection}`)  
-
+    
     if (playerSelection === computerSelection) {
       counter++
       return "Its a draw"
@@ -36,46 +84,58 @@ function game() {
     if (playerSelection == "rock") {
       if (computerSelection == "paper") {
         counter++
+        compwins++
         return "The computer wins this round!"
       }
       else {
         counter++
+        humanwins++
         return "You win this round!"
       } 
     }
     else if (playerSelection == "paper") {
       if (computerSelection == "scissors") {
         counter++
+        compwins++
         return "The computer wins this round!"
       }
       else {
         counter++
+        humanwins++
         return "You win this round!"
       }
       }
     else if (playerSelection == "scissors") {
       if (computerSelection == "paper") {
         counter++
+        compwins++
         return "The computer wins this round!"
       }
       }
   }   
   
-  getComputerChoice()
   
-  let playerSelection = prompt("Rock Paper Or scissors?").toLowerCase
+  
+ // let playerSelection = "rock"
   let computerSelection = getComputerChoice()
-  
+  playerSelection = playerSelection.toLowerCase()
   
   console.log(playRound(playerSelection, computerSelection))
   console.log(`You are on round: ${counter}`)
+  
+  console.log(`players wins = ${humanwins}`)
+  console.log(`computers wins = ${compwins}`)
+  if (counter == 5 && compwins < humanwins) {
+    console.log("The player has won best out of 5!")}
+  else if (counter == 5 && compwins > humanwins) {
+    console.log("The computer has won best out of 5!")
+    }
+  
+  
 }
 
-game()
-game()
-game()
-game()
-game()
+
+
   
 
 
